9d9d5e93c41057039aafcb0389e1b6bbc1090685
Marko Kudjerski <marko@highfidelity.io>
2018-10-26 12:06:16 -0700

    antigrav App initial commit
    The app expects JSON payload with rotation object containing x, y, z.

---
+++ antigrav/antigravApp.js
@@ -0,0 +1,60 @@
+(function() {
+    var APP_NAME = "Antigrav";
+    var APP_QML = Script.resolvePath("antigravApp.qml");
+    var APP_ICON = Script.resolvePath("3D-compass.svg");
+    var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+    var button = tablet.addButton({
+        text: APP_NAME,
+        icon: APP_ICON
+    });
+
+    function onClicked() {
+        tablet.loadQMLSource(APP_QML);
+    }
+    function orientateMe(rotate_deg_x, rotate_deg_y, rotate_deg_z) {
+        MyAvatar.orientation =
+            Quat.multiply(
+                MyAvatar.orientation,
+                Quat.fromVec3Degrees({
+                    x: rotate_deg_x,
+                    y: rotate_deg_y,
+                    z: rotate_deg_z}
+                )
+            );
+    }
+
+    function reset() {
+        MyAvatar.orientation =
+            Quat.fromVec3Degrees({
+                x: 0,
+                y: MyAvatar.orientation.safeEulerAngles(0).y,
+                z: 0
+            })
+    }
+
+    function onWebEventReceived(event) {
+        print("antigravApp.js recieved a web event: " + event);
+        
+        if (typeof event === "string") {
+            event = JSON.parse(event);
+
+            if (event.hasOwnProperty("rotation")) {
+                let rotation = JSON.parse(event.rotation);
+                let requiredProperties = ['x','y','z'];
+
+                if (requiredProperties.every(function(x) { return x in rotation; })) {
+                    orientateMe(rotation.x, rotation.y, rotation.z);
+                }
+            }
+        }
+    }
+
+    function cleanup() {
+        tablet.removeButton(button);
+        reset();
+    }
+
+    button.clicked.connect(onClicked);
+    tablet.webEventReceived.connect(onWebEventReceived);
+    Script.scriptEnding.connect(cleanup);
+}());
